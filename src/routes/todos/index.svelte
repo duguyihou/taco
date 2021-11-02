4b65c0b3a29ae732cc0fbb4c7aeb1b767445870a
kong <duguyihou@gmail.com>
2021-11-03 08:23:48 +1100

    refactor: apply tailwind to todo page

--- src/routes/todos/index.svelte
+++ src/routes/todos/index.svelte
@@ -47,12 +47,12 @@
 </script>
 
 <svelte:head>
-	<title>Taco</title>
+	<title>home</title>
 </svelte:head>
 
-<div class="w-full leading-none">
+<div class="container flex flex-col justify-start items-center">
 	<form
-		class="m-2"
+		class="w-full m-2"
 		action="/todos.json"
 		method="post"
 		use:enhance={{
@@ -65,16 +65,15 @@
 		}}
 	>
 		<input
-			class="w-full border-2 focus:shadow-lgborder-2 outline-none text-3xl px-4 py-2 rounded-lg text-left"
+			class="w-full border-2 focus:shadow-lg outline-none text-3xl px-4 py-2 rounded-lg text-left"
 			name="text"
 			aria-label="Add todo"
 			placeholder="+ tap to add a todo"
 		/>
 	</form>
-
 	{#each todos as todo (todo.uid)}
 		<div
-			class="todo"
+			class="w-full grid grid-cols-todo gap-2 items-center border-2 outline-none px-4  m-2 bg-white rounded-lg backdrop-filter -translate-x-1 translate-y-1"
 			class:done={todo.done}
 			transition:scale|local={{ start: 0.7 }}
 			animate:flip={{ duration: 200 }}
@@ -89,8 +88,16 @@
 					result: patch
 				}}
 			>
-				<input type="hidden" name="done" value={todo.done ? '' : 'true'} />
-				<button class="toggle" aria-label="Mark todo as {todo.done ? 'not done' : 'done'}" />
+				<input
+					class="flex-1 px-2 py-4 rounded-sm"
+					type="hidden"
+					name="done"
+					value={todo.done ? '' : 'true'}
+				/>
+				<button
+					class="w-8 h-8 border-0 bg-opacity-100 bg-no-repeat bg-center toggle"
+					aria-label="Mark todo as {todo.done ? 'not done' : 'done'}"
+				/>
 			</form>
 
 			<form
@@ -101,7 +108,13 @@
 					result: patch
 				}}
 			>
-				<input aria-label="Edit todo" type="text" name="text" value={todo.text} />
+				<input
+					class="w-full outline-none text-3xl px-4 py-2 rounded-lg text-left"
+					aria-label="Edit todo"
+					type="text"
+					name="text"
+					value={todo.text}
+				/>
 				<button class="save" aria-label="Save todo" />
 			</form>
 
@@ -122,20 +135,6 @@
 </div>
 
 <style>
-	.todo {
-		display: grid;
-		grid-template-columns: 2rem 1fr 2rem;
-		grid-gap: 0.5rem;
-		align-items: center;
-		margin: 0 0 0.5rem 0;
-		padding: 0.5rem;
-		background-color: white;
-		border-radius: 8px;
-		filter: drop-shadow(2px 4px 6px rgba(0, 0, 0, 0.1));
-		transform: translate(-1px, -1px);
-		transition: filter 0.2s, transform 0.2s;
-	}
-
 	.done {
 		transform: none;
 		opacity: 0.4;
@@ -149,21 +148,6 @@
 		flex: 1;
 	}
 
-	.todo input {
-		flex: 1;
-		padding: 0.5em 2em 0.5em 0.8em;
-		border-radius: 3px;
-	}
-
-	.todo button {
-		width: 2em;
-		height: 2em;
-		border: none;
-		background-color: transparent;
-		background-position: 50% 50%;
-		background-repeat: no-repeat;
-	}
-
 	button.toggle {
 		border: 1px solid rgba(0, 0, 0, 0.2);
 		border-radius: 50%;
@@ -173,29 +157,5 @@
 
 	.done .toggle {
 		background-image: url("data:image/svg+xml,%3Csvg width='22' height='16' viewBox='0 0 22 16' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M20.5 1.5L7.4375 14.5L1.5 8.5909' stroke='%23676778' stroke-width='1.5' stroke-linecap='round' stroke-linejoin='round'/%3E%3C/svg%3E");
-	}
-
-	.delete {
-		background-image: url("data:image/svg+xml,%3Csvg width='24' height='24' viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M4.5 5V22H19.5V5H4.5Z' fill='%23676778' stroke='%23676778' stroke-width='1.5' stroke-linejoin='round'/%3E%3Cpath d='M10 10V16.5' stroke='white' stroke-width='1.5' stroke-linecap='round' stroke-linejoin='round'/%3E%3Cpath d='M14 10V16.5' stroke='white' stroke-width='1.5' stroke-linecap='round' stroke-linejoin='round'/%3E%3Cpath d='M2 5H22' stroke='%23676778' stroke-width='1.5' stroke-linecap='round' stroke-linejoin='round'/%3E%3Cpath d='M8 5L9.6445 2H14.3885L16 5H8Z' fill='%23676778' stroke='%23676778' stroke-width='1.5' stroke-linejoin='round'/%3E%3C/svg%3E%0A");
-		opacity: 0.2;
-	}
-
-	.delete:hover,
-	.delete:focus {
-		transition: opacity 0.2s;
-		opacity: 1;
-	}
-
-	.save {
-		position: absolute;
-		right: 0;
-		opacity: 0;
-		background-image: url("data:image/svg+xml,%3Csvg width='24' height='24' viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M20.5 2H3.5C2.67158 2 2 2.67157 2 3.5V20.5C2 21.3284 2.67158 22 3.5 22H20.5C21.3284 22 22 21.3284 22 20.5V3.5C22 2.67157 21.3284 2 20.5 2Z' fill='%23676778' stroke='%23676778' stroke-width='1.5' stroke-linejoin='round'/%3E%3Cpath d='M17 2V11H7.5V2H17Z' fill='white' stroke='white' stroke-width='1.5' stroke-linejoin='round'/%3E%3Cpath d='M13.5 5.5V7.5' stroke='%23676778' stroke-width='1.5' stroke-linecap='round'/%3E%3Cpath d='M5.99844 2H18.4992' stroke='%23676778' stroke-width='1.5' stroke-linecap='round'/%3E%3C/svg%3E%0A");
-	}
-
-	.todo input:focus + .save,
-	.save:focus {
-		transition: opacity 0.2s;
-		opacity: 1;
 	}
 </style>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,14 +1,21 @@
 module.exports = {
-	purge: ['./src/**/*.html', './src/**/*.js'],
+	purge: ['./src/**/*.html', './src/**/*.svelte'],
 	darkMode: false, // or 'media' or 'class'
 	theme: {
 		colors: {
 			blue: {
 				DEFAULT: '#232323',
 				dark: '#2D323B'
+			},
+			white: {
+				DEFAULT: '#FFF'
 			}
 		},
-		extend: {}
+		extend: {
+			gridTemplateColumns: {
+				todo: '2rem 1fr 2rem'
+			}
+		}
 	},
 	variants: {
 		extend: {}
