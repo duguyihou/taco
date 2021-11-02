420ec13ffde089521442714c38c5d71f5ca5c5ef
kong <duguyihou@gmail.com>
2021-11-02 22:11:45 +1100

    feat(chore): some progress

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -4,42 +4,9 @@
 </script>
 
 <Header />
-
-<main>
+<main class="flex-1 flex flex-col w-full h-full box-border p-4 bg-blue-dark">
 	<slot />
 </main>
-
-<footer>
-	<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
+<footer class="absolute bottom-0 mx-auto inset-x-0">
+	<p class="text-center">taco</p>
 </footer>
-
-<style>
-	main {
-		flex: 1;
-		display: flex;
-		flex-direction: column;
-		padding: 1rem;
-		width: 100%;
-		max-width: 1024px;
-		margin: 0 auto;
-		box-sizing: border-box;
-	}
-
-	footer {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		padding: 40px;
-	}
-
-	footer a {
-		font-weight: bold;
-	}
-
-	@media (min-width: 480px) {
-		footer {
-			padding: 40px 0;
-		}
-	}
-</style>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,12 @@ module.exports = {
 	purge: ['./src/**/*.html', './src/**/*.js'],
 	darkMode: false, // or 'media' or 'class'
 	theme: {
+		colors: {
+			blue: {
+				DEFAULT: '#232323',
+				dark: '#2D323B'
+			}
+		},
 		extend: {}
 	},
 	variants: {
