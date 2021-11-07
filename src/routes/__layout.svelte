3de31ebc85e9e8244ebe4d61246625812688bf2e
kong <duguyihou@gmail.com>
2021-11-07 13:23:18 +1100

    style: change some colors

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,9 +1,7 @@
 <script lang="ts">
-	import Header from '$lib/header/Header.svelte'
 	import '../app.postcss'
 </script>
 
-<Header />
 <main class="flex-1 flex flex-row w-full h-full box-border p-4 bg-blue-light">
 	<slot />
 </main>
