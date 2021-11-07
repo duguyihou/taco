bcdab21e23b0630b3921f2ba7808b3c398847ff4
kong <duguyihou@gmail.com>
2021-11-07 22:23:44 +1100

    chore: wip

--- src/lib/selectedTodo/SelectedTodo.svelte
+++ src/lib/selectedTodo/SelectedTodo.svelte
@@ -17,13 +17,15 @@
 	</header>
 	<div class="item">
 		<input class="check" type="checkbox" {checked} on:click={() => handleCheck(selectedTodo)} />
-		<button type="button" class="value" class:checked>{text}</button>
+		<div class="value" contenteditable class:checked>
+			{text}
+		</div>
 	</div>
 </form>
 
 <style lang="postcss">
 	.form {
-		@apply container bg-grey-light h-auto p-2 text-lg rounded flex flex-col justify-start items-center shadow-sm hover:bg-grey;
+		@apply container bg-grey-light h-auto p-2 text-lg rounded flex flex-col justify-start items-center shadow-sm hover:bg-grey overflow-scroll;
 	}
 	header {
 		@apply w-full flex flex-row justify-between items-center;
@@ -32,7 +34,7 @@
 		@apply w-5 h-5 mx-2 border-2 cursor-pointer flex justify-center items-center;
 	}
 	.value {
-		@apply flex-1 text-lg p-2 rounded outline-none text-left cursor-pointer text-black-light;
+		@apply w-full text-lg p-2 rounded outline-none text-left cursor-pointer text-black-light;
 	}
 	.checked {
 		@apply line-through bg-grey-dark border-2 border-grey-dark;
