c1ab95bfd74b3314d0282a821f465e6b6ab2b59d
kong <duguyihou@gmail.com>
2021-11-12 22:32:55 +1100

    feat(selected task): init ui

--- src/lib/components/SelectedTask.svelte
+++ src/lib/components/SelectedTask.svelte
@@ -1,7 +1,24 @@
 <script lang="ts">
-	import type { Task } from '$lib/typings'
-
-	export let task: Task
+	import { selectedTask } from '$lib/store/selectedTask'
+	$: ({ priority, content } = $selectedTask)
 </script>
 
-<h1>selected task {task.content}</h1>
+<form>
+	<section class="todo">
+		<input type="checkbox" />
+		<div class="content" contenteditable bind:textContent={content} />
+		<span>{`p-${priority}`}</span>
+	</section>
+</form>
+
+<style lang="postcss">
+	form {
+		@apply container bg-grey-light w-full h-full p-2 border-none text-base flex flex-col justify-start items-center;
+	}
+	.todo {
+		@apply w-full flex flex-row justify-between items-center;
+	}
+	.content {
+		@apply flex-1;
+	}
+</style>
