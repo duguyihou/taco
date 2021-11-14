fb05f58e18f0205472ae8f18ed79a7b5297fadce
kong <duguyihou@gmail.com>
2021-11-15 08:08:20 +1100

    refactor: header & footer in selected task component

---
+++ src/lib/components/TaskHeader.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import Star from './Star.svelte'
+
+	export let content: string
+	export let priority: number
+</script>
+
+<section>
+	<input type="checkbox" />
+	<div contenteditable bind:textContent={content} />
+	<Star {priority} />
+</section>
+
+<style lang="postcss">
+	section {
+		@apply w-full py-2 flex flex-row justify-between items-center;
+	}
+
+	input {
+		@apply w-4 h-4 mx-2;
+	}
+	div {
+		@apply flex-1;
+	}
+</style>
