78d0b327e005857db890aedd8a8ea8673cb10de3
kong <duguyihou@gmail.com>
2021-11-15 21:39:10 +1100

    feat: add check function

---
+++ src/lib/components/Checkbox.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import { closeTask, reopenTask } from '$lib/store/selectedTask'
+	import type { Task } from '$lib/typings'
+	export let task: Task
+	export let completed: boolean
+	async function handleCheck(completed: boolean) {
+		completed ? await closeTask(task) : await reopenTask(task)
+	}
+</script>
+
+<input type="checkbox" bind:checked={completed} on:change={() => handleCheck(completed)} />
+
+<style lang="postcss">
+	input {
+		@apply w-4 h-4 mx-2;
+	}
+</style>
